f0a949b82d51119c7b2be0039365920feccd0141
JanD <>
2024-11-10 16:03:27 +0000

    hit test on drawing
    FossilOrigin-Name: 72d29ae3ec697d91a4bbd1fce917f0459614f8ff7783f4a1d6620954295b0df6

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 import {CompositeFigure} from './figures.js';
 import {Rect} from './geom.js';
-import {findFiguresBelowPoint, findEnclosingFigures, findEnclosedFigures} from './hitTest.js';
+import {findFiguresBelowPoint, findEnclosingFigures, findEnclosedFigures, findFiguresEnclosingAndEnclosed} from './hitTest.js';
 
 // the drawing contains other figures, so it is basically a composite figure
 class Drawing extends CompositeFigure{
@@ -9,25 +9,45 @@ class Drawing extends CompositeFigure{
         this.setRect(new Rect({
             "x":0,
             "y":0,
-            "width":1000,
-            "height":1000
+            "width":600,
+            "height":600
         }));
     }
     draw(ctx){
+        const {width, height} = this.getRect();
+        ctx.save();
+        ctx.fillStyle = "lightgray";
+        ctx.fillRect(0,0,width,height);
+        ctx.restore()
         this.drawContainedFigures(ctx);
     }
     //#region hit tests
+    /**
+     * Convenience method for the most common use for hit-testing figures: 
+     * Finding the innermost enclosing figure and the enclosed figures that are 
+     * enclosed in this enclosing figures (which would then attached to the figures passed for hit-testing)
+     * 
+     * @param {Rect} figure 
+     * @returns {Object} 
+     */
+    findFiguresEnclosingAndEnclosed(rect){
+        //unpacking the variables and repackaging again does not make much sense, but I want to show what is in the object. lets see how to do this better. 
+        const {rectEnclosesFigures,rectEnclosedByFigure} = findFiguresEnclosingAndEnclosed(this,rect);
+
+        return {
+            "rectEnclosesFigures":  rectEnclosesFigures,
+            "rectEnclosedByFigure": rectEnclosedByFigure
+        };
+    }
+
     findFiguresEnclosingPoint(point){
-       const figuresBelowPoint = findFiguresBelowPoint(this,point,false);
+       const figuresBelowPoint = findFiguresBelowPoint(this,point,false); //…,…,false = don’t allow to select the drawing itself
        return figuresBelowPoint;
     }
-    findFiguresEnclosingFigure(figure){
-        const enclosingFigures = findEnclosingFigures(this,figure, false);
-        return enclosingFigures;
-    }
-    findFiguresEnclosedByFigure(figure){
-        const enclosedFigures = findEnclosedFigures(this,figure,false);
-        return enclosedFigures;
+    toJSON(){
+        return{
+            "containedFigures":this.getJsonOfContainedFigures()
+        }
     }
 }
 
